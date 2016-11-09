546f4cbb20a0488be1393f2f9ce25e223f929b1a
Mark Weigl <markwco@icloud.com>
2016-11-08 21:22:07 -0700

    project complete

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-// your code here!
+$(document).ready(function(){
+	$("#myform").submit(function(event){
+		event.preventDefault();
+		var words;
+		words=$("#user-text").val().toLowerCase().split(" ");
+		console.log(words);
+		$("#wordCount").text(words.length);
+		uniqueWordCount(words);
+		$("dl").removeClass("hidden");
+	});
+});
+
+function uniqueWordCount(words) {
+  	var uniqueWords = {};
+  	var totalCharacters = 0;
+  	for (var i = 0; i < words.length; i++) {
+    	if (words[i] in uniqueWords) {
+       		uniqueWords[words[i]]++;
+    	}
+    	else {
+      		uniqueWords[words[i]]=1;
+    	}
+ 		totalCharacters += words[i].length;
+ 	}
+ 	$("#avgWordLength").text(totalCharacters / words.length);
+ 	$("#uniqueWordCount").text(parseInt(Object.keys(uniqueWords).length));
+ 	
+}
